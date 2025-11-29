c109c04eb3c7c9dedcfbd3ffa37a7a1f28c6d725
Jack Darnell <darnelljack@icloud.com>
2025-11-29 14:30:50 -0500

    FFmpeg 7 Compatibility Fix

--- installer/scripts/install.sh
+++ installer/scripts/install.sh
@@ -11,7 +11,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 # Configuration
-SHAIRPORT_VERSION="4.3.2"
+SHAIRPORT_VERSION="development"  # Use development branch for FFmpeg 7 compatibility fix
 INSTALL_DIR="/opt/airsync"
 SERVICE_USER="airsync"
 CONFIG_DIR="/etc/airsync"
